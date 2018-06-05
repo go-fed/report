0c2af0680d1ab6a6297adec56f3da896f7370697
Cory Slep <coryslep1@mac.com>
2018-06-05 23:27:57 +0200

    Fixes for the c2s implementation report

--- app.go
+++ app.go
@@ -8,6 +8,7 @@ import (
 	"github.com/go-fed/activity/streams"
 	"github.com/go-fed/activity/vocab"
 	"github.com/go-fed/httpsig"
+	"log"
 	"net/http"
 	"net/url"
 	"sync"
@@ -20,60 +21,122 @@ var _ pub.SocialApplication = &app{}
 var _ pub.FederateApplication = &app{}
 var _ pub.SocialFederateApplication = &app{}
 
+type lockKeyType string
+
 type lockObj struct {
 	obj pub.PubObject
 	mu  *sync.RWMutex
+	who int
 }
 
 // app shows the basic mechanics for a single-user, non-permanent, dummy server.
 type app struct {
-	scheme    string
-	host      string
-	newPath   string
-	db        map[string]lockObj
-	dbMu      *sync.RWMutex
-	actor     *vocab.Object
-	actorMu   *sync.RWMutex
-	actorURL  *url.URL
-	inboxURL  *url.URL
-	outboxURL *url.URL
-	id        int
-	idMu      *sync.Mutex
-	pubKey    crypto.PublicKey
-	privKey   crypto.PrivateKey
-	verifier  pub.SocialAPIVerifier
+	scheme       string
+	host         string
+	newPath      string
+	db           map[string]*lockObj
+	dbMu         *sync.RWMutex
+	actor        *vocab.Person
+	actorURL     *url.URL
+	inboxURL     *url.URL
+	outboxURL    *url.URL
+	followingURL *url.URL
+	followersURL *url.URL
+	likedURL     *url.URL
+	inbox        vocab.OrderedCollectionType
+	inboxMu      *sync.RWMutex
+	outbox       vocab.OrderedCollectionType
+	outboxMu     *sync.RWMutex
+	following    vocab.OrderedCollectionType
+	followingMu  *sync.RWMutex
+	followers    vocab.OrderedCollectionType
+	followersMu  *sync.RWMutex
+	liked        vocab.OrderedCollectionType
+	likedMu      *sync.RWMutex
+	id           int
+	idMu         *sync.Mutex
+	pubKey       crypto.PublicKey
+	privKey      crypto.PrivateKey
+	verifier     pub.SocialAPIVerifier
 }
 
-func newApp(scheme, host, newPath string, actorURL, inboxURL, outboxURL *url.URL, pubKey crypto.PublicKey, privKey crypto.PrivateKey, actor *vocab.Object, verifier pub.SocialAPIVerifier) *app {
+func newApp(scheme, host, newPath string, actorURL, inboxURL, outboxURL, followingURL, followersURL, likedURL *url.URL, pubKey crypto.PublicKey, privKey crypto.PrivateKey, actor *vocab.Person, verifier pub.SocialAPIVerifier) *app {
+	inbox := &vocab.OrderedCollection{}
+	inbox.SetId(inboxURL)
+	outbox := &vocab.OrderedCollection{}
+	outbox.SetId(outboxURL)
+	following := &vocab.OrderedCollection{}
+	following.SetId(followingURL)
+	followers := &vocab.OrderedCollection{}
+	followers.SetId(followersURL)
+	liked := &vocab.OrderedCollection{}
+	liked.SetId(likedURL)
 	return &app{
-		scheme:    scheme,
-		host:      host,
-		newPath:   newPath,
-		db:        make(map[string]lockObj),
-		dbMu:      &sync.RWMutex{},
-		actor:     actor,
-		actorMu:   &sync.RWMutex{},
-		actorURL:  actorURL,
-		inboxURL:  inboxURL,
-		outboxURL: outboxURL,
-		id:        1,
-		idMu:      &sync.Mutex{},
-		pubKey:    pubKey,
-		privKey:   privKey,
-		verifier:  verifier,
+		scheme:       scheme,
+		host:         host,
+		newPath:      newPath,
+		db:           make(map[string]*lockObj),
+		dbMu:         &sync.RWMutex{},
+		actor:        actor,
+		actorURL:     actorURL,
+		inboxURL:     inboxURL,
+		outboxURL:    outboxURL,
+		followingURL: followingURL,
+		followersURL: followersURL,
+		likedURL:     likedURL,
+		inbox:        inbox,
+		inboxMu:      &sync.RWMutex{},
+		outbox:       outbox,
+		outboxMu:     &sync.RWMutex{},
+		following:    following,
+		followingMu:  &sync.RWMutex{},
+		followers:    followers,
+		followersMu:  &sync.RWMutex{},
+		liked:        liked,
+		likedMu:      &sync.RWMutex{},
+		id:           1,
+		idMu:         &sync.Mutex{},
+		pubKey:       pubKey,
+		privKey:      privKey,
+		verifier:     verifier,
 	}
 }
 
 func (a *app) Owns(c context.Context, id *url.URL) bool {
+	log.Printf("Owns: %s", id)
 	return id.Host == a.host
 }
 
 func (a *app) Get(c context.Context, id *url.URL, rw pub.RWType) (pub.PubObject, error) {
+	log.Printf("Getting: %s", id)
 	has, err := a.Has(c, id)
 	if err != nil {
 		return nil, err
 	} else if !has {
 		return nil, fmt.Errorf("%s not found", id)
+	}
+	if *id == *a.actorURL {
+		return a.actor, nil
+	} else if *id == *a.inboxURL {
+		a.inboxMu.RLock()
+		defer a.inboxMu.RUnlock()
+		return a.inbox, nil
+	} else if *id == *a.outboxURL {
+		a.outboxMu.RLock()
+		defer a.outboxMu.RUnlock()
+		return a.outbox, nil
+	} else if *id == *a.followingURL {
+		a.followingMu.RLock()
+		defer a.followingMu.RUnlock()
+		return a.following, nil
+	} else if *id == *a.followersURL {
+		a.followersMu.RLock()
+		defer a.followersMu.RUnlock()
+		return a.followers, nil
+	} else if *id == *a.likedURL {
+		a.likedMu.RLock()
+		defer a.likedMu.RUnlock()
+		return a.liked, nil
 	}
 	a.dbMu.RLock()
 	p, ok := a.db[id.String()]
@@ -84,16 +147,21 @@ func (a *app) Get(c context.Context, id *url.URL, rw pub.RWType) (pub.PubObject,
 	switch rw {
 	case pub.Read:
 		p.mu.RLock()
-		go func() {
-			<-c.Done()
-			p.mu.RUnlock()
-		}()
+		defer p.mu.RUnlock()
 	case pub.ReadWrite:
-		p.mu.Lock()
-		go func() {
-			<-c.Done()
-			p.mu.Unlock()
-		}()
+		who := c.Value(lockKeyType("lockKey")).(int)
+		if p.who != who {
+			log.Printf("locking %s", id)
+			p.mu.Lock()
+			p.who = who
+			go func() {
+				<-c.Done()
+				if p.who == who {
+					log.Printf("unlocking %s", id)
+					p.mu.Unlock()
+				}
+			}()
+		}
 	default:
 		return nil, fmt.Errorf("unrecognized pub.RWType: %v", rw)
 	}
@@ -101,60 +169,112 @@ func (a *app) Get(c context.Context, id *url.URL, rw pub.RWType) (pub.PubObject,
 }
 
 func (a *app) GetAsVerifiedUser(c context.Context, id, authdUser *url.URL, rw pub.RWType) (pub.PubObject, error) {
+	log.Printf("GetAsVerifiedUser: %s", id)
 	return a.Get(c, id, rw)
 }
 
 func (a *app) Has(c context.Context, id *url.URL) (bool, error) {
+	log.Printf("Has: %s", id)
+	if *id == *a.actorURL || *id == *a.inboxURL || *id == *a.outboxURL || *id == *a.followingURL || *id == *a.followersURL || *id == *a.likedURL {
+		return true, nil
+	}
 	_, ok := a.db[id.String()]
 	return ok, nil
 }
 
 func (a *app) Set(c context.Context, o pub.PubObject) error {
+	b, _ := o.Serialize()
+	log.Printf("Setting: %s", b)
 	if id := o.GetId(); id == nil {
 		return fmt.Errorf("id is nil")
 	} else if *id == *a.outboxURL {
-		a.actorMu.Lock()
-		defer a.actorMu.Unlock()
+		a.outboxMu.Lock()
+		defer a.outboxMu.Unlock()
 		oc, ok := o.(vocab.OrderedCollectionType)
 		if !ok {
 			return fmt.Errorf("setting %s but not an OrderedCollectionType", id)
 		}
-		a.actor.SetOutboxOrderedCollection(oc)
+		a.outbox = oc
 		return nil
 	} else if *id == *a.inboxURL {
-		a.actorMu.Lock()
-		defer a.actorMu.Unlock()
+		a.inboxMu.Lock()
+		defer a.inboxMu.Unlock()
 		oc, ok := o.(vocab.OrderedCollectionType)
 		if !ok {
 			return fmt.Errorf("setting %s but not an OrderedCollectionType", id)
 		}
-		a.actor.SetInboxOrderedCollection(oc)
+		a.inbox = oc
+		return nil
+	} else if *id == *a.followingURL {
+		a.followingMu.Lock()
+		defer a.followingMu.Unlock()
+		oc, ok := o.(vocab.OrderedCollectionType)
+		if !ok {
+			return fmt.Errorf("setting %s but not an OrderedCollectionType", id)
+		}
+		a.following = oc
+		return nil
+	} else if *id == *a.followersURL {
+		a.followersMu.Lock()
+		defer a.followersMu.Unlock()
+		oc, ok := o.(vocab.OrderedCollectionType)
+		if !ok {
+			return fmt.Errorf("setting %s but not an OrderedCollectionType", id)
+		}
+		a.followers = oc
+		return nil
+	} else if *id == *a.likedURL {
+		a.likedMu.Lock()
+		defer a.likedMu.Unlock()
+		oc, ok := o.(vocab.OrderedCollectionType)
+		if !ok {
+			return fmt.Errorf("setting %s but not an OrderedCollectionType", id)
+		}
+		a.liked = oc
 		return nil
 	} else {
-		a.dbMu.RLock()
-		a.db[id.String()] = lockObj{
-			obj: o,
-			mu:  &sync.RWMutex{},
+		a.dbMu.Lock()
+		if v, ok := a.db[id.String()]; ok {
+			a.dbMu.Unlock()
+			who := c.Value(lockKeyType("lockKey")).(int)
+			vWho := v.who
+			// TODO: Use sync.Cond
+			if vWho == 0 || vWho != who {
+				log.Printf("locking %s", id)
+				v.mu.Lock()
+				v.who = who
+			}
+			v.obj = o
+			v.who = 0
+			log.Printf("unlocking %s", id)
+			v.mu.Unlock()
+		} else {
+			a.db[id.String()] = &lockObj{
+				obj: o,
+				mu:  &sync.RWMutex{},
+			}
+			a.dbMu.Unlock()
 		}
-		a.dbMu.RUnlock()
 		return nil
 	}
 }
 
 func (a *app) GetInbox(c context.Context, r *http.Request, rw pub.RWType) (vocab.OrderedCollectionType, error) {
+	log.Printf("GetInbox: %s", r.URL)
 	if *r.URL == *a.inboxURL {
-		a.actorMu.RLock()
-		defer a.actorMu.RUnlock()
-		return a.actor.GetInboxOrderedCollection(), nil
+		a.inboxMu.RLock()
+		defer a.inboxMu.RUnlock()
+		return a.inbox, nil
 	}
 	return nil, fmt.Errorf("no inbox for url %s", r.URL)
 }
 
 func (a *app) GetOutbox(c context.Context, r *http.Request, rw pub.RWType) (vocab.OrderedCollectionType, error) {
+	log.Printf("GetOutbox: %s", r.URL)
 	if *r.URL == *a.outboxURL {
-		a.actorMu.RLock()
-		defer a.actorMu.RUnlock()
-		return a.actor.GetOutboxOrderedCollection(), nil
+		a.outboxMu.RLock()
+		defer a.outboxMu.RUnlock()
+		return a.outbox, nil
 	}
 	return nil, fmt.Errorf("no outbox for url %s", r.URL)
 }
@@ -188,6 +308,7 @@ func (a *app) CanRemove(c context.Context, o vocab.ObjectType, t vocab.ObjectTyp
 }
 
 func (a *app) ActorIRI(c context.Context, r *http.Request) (*url.URL, error) {
+	log.Printf("ActorIRI: %s", r.URL)
 	if *r.URL == *a.inboxURL || *r.URL == *a.outboxURL {
 		return a.actorURL, nil
 	}
--- report.go
+++ report.go
@@ -10,14 +10,18 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"sync"
 )
 
 const (
-	actorPath  = "/actor"
-	inboxPath  = "/actor/inbox"
-	outboxPath = "/actor/outbox"
-	authPath   = "/auth"
-	tokenPath  = "/token"
+	actorPath     = "/actor"
+	inboxPath     = "/actor/inbox"
+	outboxPath    = "/actor/outbox"
+	followingPath = "/actor/following"
+	followersPath = "/actor/followers"
+	likedPath     = "/actor/liked"
+	authPath      = "/auth"
+	tokenPath     = "/token"
 )
 
 // SetReportMux builds a basic Social API and Federate API server using the
@@ -42,6 +46,18 @@ func SetReportMux(m *http.ServeMux, scheme, host, newPath string) error {
 	if err != nil {
 		return err
 	}
+	followingURL, err := url.Parse(fmt.Sprintf("%s://%s%s", scheme, host, followingPath))
+	if err != nil {
+		return err
+	}
+	followersURL, err := url.Parse(fmt.Sprintf("%s://%s%s", scheme, host, followersPath))
+	if err != nil {
+		return err
+	}
+	likedURL, err := url.Parse(fmt.Sprintf("%s://%s%s", scheme, host, likedPath))
+	if err != nil {
+		return err
+	}
 	authURL, err := url.Parse(fmt.Sprintf("%s://%s%s", scheme, host, authPath))
 	if err != nil {
 		return err
@@ -58,15 +74,15 @@ func SetReportMux(m *http.ServeMux, scheme, host, newPath string) error {
 	endpoints := &vocab.Object{}
 	endpoints.SetOauthAuthorizationEndpoint(authURL)
 	endpoints.SetOauthTokenEndpoint(tokenURL)
-	actor := &vocab.Object{}
+	actor := &vocab.Person{}
 	actor.SetEndpoints(endpoints)
 	actor.SetId(actorURL)
 	actor.AppendNameString("Implementation Report Account")
-	actor.SetOutboxOrderedCollection(&vocab.OrderedCollection{})
-	actor.SetInboxOrderedCollection(&vocab.OrderedCollection{})
-	actor.SetFollowingCollection(&vocab.Collection{})
-	actor.SetFollowersCollection(&vocab.Collection{})
-	actor.SetLikedCollection(&vocab.Collection{})
+	actor.SetOutboxAnyURI(outboxURL)
+	actor.SetInboxAnyURI(inboxURL)
+	actor.SetFollowingAnyURI(followingURL)
+	actor.SetFollowersAnyURI(followersURL)
+	actor.SetLikedAnyURI(likedURL)
 	actor.SetPreferredUsername("Implementation Report Account")
 
 	// Prepare basic implementation
@@ -74,18 +90,36 @@ func SetReportMux(m *http.ServeMux, scheme, host, newPath string) error {
 		ActorURL:  actorURL,
 		OutboxURL: outboxURL,
 	}
-	app := newApp(scheme, host, newPath, actorURL, inboxURL, outboxURL, pubKey, privKey, actor, verifier)
+	app := newApp(scheme, host, newPath, actorURL, inboxURL, outboxURL, followingURL, followersURL, likedURL, pubKey, privKey, actor, verifier)
 	fedCb := &nothingCallbacker{}
 	socialCb := &nothingCallbacker{}
 	clock := &localClock{}
 	deliverer := &syncDeliverer{}
 	pubber := pub.NewPubber(clock, app, socialCb, fedCb, deliverer, &http.Client{}, "go-fed-report", 5, 5)
 	serveFn := pub.ServeActivityPubObject(app, clock)
+	addMissingFn := func(r *http.Request) {
+		r.URL.Host = host
+		r.URL.Scheme = scheme
+	}
 
+	// Set up sync primitives
+	lockKey := 1
+	lockKeyMu := &sync.Mutex{}
+	getLockKeySafely := func() (context.Context, context.CancelFunc) {
+		c := context.Background()
+		k := lockKeyType("lockKey")
+		lockKeyMu.Lock()
+		defer lockKeyMu.Unlock()
+		v := lockKey
+		lockKey++
+		return context.WithCancel(context.WithValue(c, k, v))
+	}
 	// Set up handlers
 	m.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		addMissingFn(r)
 		log.Printf("received request to %q", r.URL)
-		c := context.Background()
+		c, cfn := getLockKeySafely()
+		defer cfn()
 		if handled, err := serveFn(c, w, r); err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			log.Print(err)
@@ -94,11 +128,14 @@ func SetReportMux(m *http.ServeMux, scheme, host, newPath string) error {
 			return
 		}
 		log.Printf("request to %q not an activitypub request", r.URL)
+		log.Print(r)
 		w.WriteHeader(http.StatusNotFound)
 	})
 	m.HandleFunc(actorPath, func(w http.ResponseWriter, r *http.Request) {
+		addMissingFn(r)
 		log.Printf("received request to %q", r.URL)
-		c := context.Background()
+		c, cfn := getLockKeySafely()
+		defer cfn()
 		if handled, err := serveFn(c, w, r); err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			log.Print(err)
@@ -107,11 +144,14 @@ func SetReportMux(m *http.ServeMux, scheme, host, newPath string) error {
 			return
 		}
 		log.Printf("request to %q not an activitypub request", r.URL)
+		log.Print(r)
 		w.WriteHeader(http.StatusNotFound)
 	})
 	m.HandleFunc(inboxPath, func(w http.ResponseWriter, r *http.Request) {
+		addMissingFn(r)
 		log.Printf("received request to %q", r.URL)
-		c := context.Background()
+		c, cfn := getLockKeySafely()
+		defer cfn()
 		if handled, err := pubber.GetInbox(c, w, r); err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			log.Print(err)
@@ -127,11 +167,14 @@ func SetReportMux(m *http.ServeMux, scheme, host, newPath string) error {
 			return
 		}
 		log.Printf("request to %q not an activitypub request", r.URL)
+		log.Print(r)
 		w.WriteHeader(http.StatusNotFound)
 	})
 	m.HandleFunc(outboxPath, func(w http.ResponseWriter, r *http.Request) {
+		addMissingFn(r)
 		log.Printf("received request to %q", r.URL)
-		c := context.Background()
+		c, cfn := getLockKeySafely()
+		defer cfn()
 		if handled, err := pubber.GetOutbox(c, w, r); err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			log.Print(err)
@@ -147,13 +190,16 @@ func SetReportMux(m *http.ServeMux, scheme, host, newPath string) error {
 			return
 		}
 		log.Printf("request to %q not an activitypub request", r.URL)
+		log.Print(r)
 		w.WriteHeader(http.StatusNotFound)
 	})
 	m.HandleFunc(authPath, func(w http.ResponseWriter, r *http.Request) {
+		addMissingFn(r)
 		log.Printf("received request to %q", r.URL)
 		verifier.AuthorizeRequestWithoutActuallyDoingAnything(w, r)
 	})
 	m.HandleFunc(tokenPath, func(w http.ResponseWriter, r *http.Request) {
+		addMissingFn(r)
 		log.Printf("received request to %q", r.URL)
 		verifier.GrantBearerTokenWithoutActuallyDoingAnything(w, r)
 	})
